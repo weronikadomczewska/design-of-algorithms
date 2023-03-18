fc99e48fafb6793d65b8fa06f47a53667ceaf7c9
weronikadomczewska <w.domczewska@gmail.com>
2023-03-18 15:06:14 +0100

    ex1 and turing machine class ready

---
+++ list3/ex1.py
@@ -0,0 +1,59 @@
+from turing_machine import *
+
+beginning_state = "q0"
+acceptance_state = "qa"
+rejection_state = "qr"
+tape_alphabet = ["a", "crossed_a", "a_with_dot", "blank_sign"]
+transition_function = {
+    "a": {
+        "q0": ("q1", "a_with_dot", "R"),
+        "q1": ("q2", None, "L"),
+        "q3": ("q4", None, "R"),
+        "q4": ("q5", "crossed_a", "R"),
+        "q5": ("q3", "crossed_a", "R"),
+        "q6": ("q6", None, "L"),
+        "qa": ("qa", None, None),
+        "qr": ("qr", None, None)
+    },
+    "a_with_dot": {
+        "q2": ("q3", None, "L"),
+        "q3": ("q4", None, "R"),
+        "q6": ("q1", None, "R"),
+        "qa": ("qa", None, None),
+        "qr": ("qr", None, None)
+    },
+    "crossed_a": {
+        "q1": ("q1", None, "R"),
+        "q2": ("q2", None, "L"),
+        "q3": ("q3", None, "R"),
+        "q4": ("q4", None, "R"),
+        "q5": ("q5", None, "R"),
+        "q6": ("q6", None, "L"),
+        "qa": ("qa", None, None),
+        "qr": ("qr", None, None)
+    },
+    "blank_sign": {
+        "q0": ("qr", None, "L"),
+        "q1": ("qa", None, "L"),
+        "q3": ("q6", None, "L"),
+        "q4": ("qr", None, "L"),
+        "q5": ("qr", None, "L"),
+        "qa": ("qa", None, None),
+        "qr": ("qr", None, None)
+    }
+}
+
+turing_machine = TuringMachine(beginning_state,
+                               acceptance_state,
+                               rejection_state,
+                               tape_alphabet,
+                               transition_function)
+
+text = input("insert text: ")
+print(text)
+while not TuringMachine.check_text(text, turing_machine.tape_alphabet):
+    text = input("Insert text again: ")
+
+transitions = turing_machine.simulate_turing_machine(text)
+for t in transitions:
+    print(t)
---
+++ list3/turing_machine.py
@@ -0,0 +1,58 @@
+class TuringMachine:
+
+    def __init__(self, beginning_state,
+                 acceptance_state,
+                 rejection_state,
+                 tape_alphabet,
+                 transition_function):
+        self.tape = []
+        self.head_idx = 0
+        self.beginning_state = beginning_state
+        self.acceptance_state = acceptance_state
+        self.rejection_state = rejection_state
+        self.tape_alphabet = tape_alphabet
+        self.transition_function = transition_function
+
+    def simulate_turing_machine(self, text):
+        state = self.beginning_state
+        self.tape = text.split() + ["blank_sign"]
+        transitions = []
+        while True:
+            prev_state = state
+            prev_tape = self.tape[:]
+            prev_head_idx = self.head_idx
+            current_sign = self.tape[self.head_idx]
+            transition_details = self.transition_function[current_sign][prev_state]
+            next_state = transition_details[0]
+            next_sign = transition_details[1]
+            next_move_direction = transition_details[2]
+            if next_sign is not None:
+                self.tape[self.head_idx] = next_sign
+            if next_move_direction == "L":
+                if self.head_idx >= 1:
+                    self.head_idx -= 1
+            elif next_move_direction == "R":
+                self.head_idx += 1
+            state = next_state
+            transitions.append(f"transition: ({prev_state}, {current_sign})->({next_state}, {next_sign}, {next_move_direction}), \n"
+                               f"previous tape: {prev_tape}, \nnew_tape: {self.tape}, \n"
+                               f"previous head position: {prev_head_idx}, next head position: {self.head_idx}" + "\n")
+            if state == self.acceptance_state:
+                print(f"string {text} has been accepted")
+                break
+            elif state == self.rejection_state:
+                print(f"string {text} has been rejected")
+                break
+
+        try:
+            return transitions[:-1]
+        except KeyError:
+            print("Transitions is empty")
+
+    @staticmethod
+    def check_text(text, alphabet):
+        for sign in text.split():
+            if sign not in alphabet:
+                return False
+        return True
+
