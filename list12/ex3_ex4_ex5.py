9e6182f5d7b787a667a53c95a8f4947468beff78
weronikadomczewska <w.domczewska@gmail.com>
2023-06-03 13:24:08 +0200

    not sure if edmonds_karp works correctly

--- list12/ex3_ex4_ex5.py
+++ list12/ex3_ex4_ex5.py
@@ -18,6 +18,15 @@ class WeightedGraph:
     def get_all_nodes(self):
         return list(self.graph.keys())
 
+    # def get_augmenting_path(self, parent, source, sink):
+    #     path = []
+    #     v = sink
+    #     while v != source:
+    #         path.append(v)
+    #         v = parent[v]
+    #     path.append(source)
+    #     return path[::-1]
+
     def dijkstra(self, start):
         distances = {node: float("inf") for node in self.graph}
         distances[start] = 0
@@ -100,7 +109,7 @@ class WeightedGraph:
             x = self.find(parent, u)
             y = self.find(parent, v)
 
-            if x!= y:
+            if x != y:
                 minimum_spanning_tree.append((u, v, weight))
                 self.union(parent, rank, x, y)
                 self.visualize_kruskal_step(u, v)
@@ -121,8 +130,8 @@ class WeightedGraph:
         nx.draw_networkx_edges(G, pos, edgelist=[(u, v)], edge_color='red', width=2)
 
         plt.axis('off')
-        plt.pause(0.75)
-        plt.clf()
+        plt.pause(2.0)
+        # plt.clf()
 
     def visualize_minimum_spanning_tree(self, minimum_spanning_tree):
         G = nx.Graph()
@@ -137,6 +146,117 @@ class WeightedGraph:
 
         plt.axis('off')
         plt.show()
+
+    def bfs(self, source, sink, parent):
+        visited = set()
+        queue = [(source, float("inf"))]
+        visited.add(source)
+        while queue:
+            node, flow = queue.pop(0)
+            for neighbor, capacity in self.get_neighbors(node):
+                if neighbor not in visited and capacity > 0:
+                    visited.add(neighbor)
+                    parent[neighbor] = (node, capacity)
+                    min_flow = min(flow, capacity)
+                    if neighbor == sink:
+                        return min_flow
+                    queue.append((neighbor, min_flow))
+        return 0
+
+    def edmonds_karp(self, source, sink):
+        parent = {}
+        max_flow = 0
+        while True:
+            min_flow = self.bfs(source, sink, parent)
+            if min_flow == 0:
+                break
+            max_flow += min_flow
+            node = sink
+            while node != source:
+                prev_node, capacity = parent[node]
+                for i, (v, w) in enumerate(self.graph[prev_node]):
+                    if v == node:
+                        self.graph[prev_node][i] = (v, w - min_flow)
+                        break
+                for i, (v, w) in enumerate(self.graph[node]):
+                    if v == prev_node:
+                        self.graph[node][i] = (v, w + min_flow)
+                        break
+                node = prev_node
+            self.visualize_edmonds_karp(source, sink, parent)
+        return max_flow
+
+    def visualize_edmonds_karp(self, source, sink, parent):
+        G = nx.DiGraph()
+        G.add_weighted_edges_from([(u, v, weight) for u, neighbors in self.graph.items() for v, weight in neighbors])
+
+        node_colors = ['lightgray' for _ in G.nodes()]
+        edge_colors = ['gray' for _ in G.edges()]
+        edge_labels = {(u, v): str(w) for u, neighbors in self.graph.items() for v, w in neighbors}
+
+        pos = nx.spring_layout(G)
+        nx.draw_networkx(G, pos, with_labels=True, node_color=node_colors, font_size=8, edge_color=edge_colors)
+        nx.draw_networkx_edges(G, pos, edgelist=[(u, v) for u, v in parent.values()], edge_color='red', width=2)
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=6)
+
+        # Highlight the flow path
+        path_edges = []
+        node = sink
+        while node != source:
+            prev_node, _ = parent[node]
+            path_edges.append((prev_node, node))
+            node = prev_node
+        nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='blue', width=2)
+
+        plt.axis('off')
+        plt.show()
+
+    # def edmonds_karp(self, source, sink):
+    #     parent = {}
+    #     max_flow = 0
+    #     while True:
+    #         min_flow = self.bfs(source, sink, parent)
+    #         if min_flow == 0:
+    #             break
+    #         max_flow += min_flow
+    #         node = sink
+    #         while node != source:
+    #             prev_node, capacity = parent[node]
+    #             for i, (v, w) in enumerate(self.graph[prev_node]):
+    #                 if v == node:
+    #                     self.graph[prev_node][i] = (v, w - min_flow)
+    #                     break
+    #             for i, (v, w) in enumerate(self.graph[node]):
+    #                 if v == prev_node:
+    #                     self.graph[node][i] = (v, w + min_flow)
+    #                     break
+    #             node = prev_node
+    #         self.visualize_edmonds_karp_step(source, sink, parent)
+    #     return max_flow
+    #
+    # def visualize_edmonds_karp_step(self, source, sink, parent):
+    #     G = nx.Graph()
+    #     G.add_weighted_edges_from([(u, v, weight) for u, neighbors in self.graph.items() for v, weight in neighbors])
+    #
+    #     node_colors = ['lightgray' for _ in G.nodes()]
+    #     edge_colors = ['gray' for _ in G.edges()]
+    #     edge_labels = {(u, v): str(w) for u, neighbors in self.graph.items() for v, w in neighbors}
+    #
+    #     # Build the augmenting path
+    #     augmenting_path = []
+    #     node = sink
+    #     while node != source:
+    #         prev_node, _ = parent[node]
+    #         augmenting_path.append((prev_node, node))
+    #         node = prev_node
+    #
+    #     pos = nx.spring_layout(G)
+    #     nx.draw_networkx(G, pos, with_labels=True, node_color=node_colors, font_size=8, edge_color=edge_colors)
+    #     nx.draw_networkx_edges(G, pos, edgelist=augmenting_path, edge_color='red', width=2)
+    #     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=6)
+    #
+    #     plt.axis('off')
+    #     plt.show()
 
 
 if __name__ == "__main__":
@@ -168,12 +288,20 @@ if __name__ == "__main__":
             graph.add_edge(node, neighbor, weights[i])
             i += 1
 
+    # ex3
     # graph.visualise_dijkstra(1, 10)
-    mst = graph.kruskal()
 
-    for u, v, weight in mst:
-        graph.visualize_kruskal_step(u, v)
-    graph.visualize_minimum_spanning_tree(mst)
+    # ex4
+    # mst = graph.kruskal()
+    # for u, v, weight in mst:
+    #     graph.visualize_kruskal_step(u, v)
+    # graph.visualize_minimum_spanning_tree(mst)
+
+    # ex5
+    max_flow = graph.edmonds_karp(1, 7)
+    print("max flow: ", max_flow)
+
+
 
 
 
